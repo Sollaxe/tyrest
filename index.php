ee613310959bca0c9c28e81ba784d027d663e959
Lxer77 <alex.spartec@gmail.com>
2020-01-29 15:01:52 +0700

    Добавил стили с размерами текста и стили с различными шрифтами Подключил всю эту радость в сборку, вроде работает

--- index.php
+++ index.php
@@ -6,10 +6,15 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>tyrest</title>
-    <link rel="stylesheet" href="style/main.css">
+
+    <link rel="stylesheet" href="/style/fonts/font-css.css">
+    <link rel="stylesheet" href="/style/main.css">
+
+    <link rel="stylesheet" href="/style/builds/index.build/desktop.css" media="only screen and (min-width: 769px)">
+    <link rel="stylesheet" href="/style/builds/index.build/tablet.css" media="only screen and (min-width: 451px) and (max-width: 768px)">
+    <link rel="stylesheet" href="/style/builds/index.build/mobile.css" media="only screen (max-width: 450px)">
 </head>
 <body>
     <h1>hello world</h1>
-
 </body>
 </html>
