318ad625e1465d33627819ca937bf30a08361ae7
Lxer77 <alex.spartec@gmail.com>
2020-01-30 01:24:25 +0700

    Сверстал хэдер с панелью навигации и слайдером Сделал несколько необходимых для работы блоков Начал юзать grid

--- index.php
+++ index.php
@@ -10,12 +10,52 @@
     <link rel="stylesheet" href="/style/fonts/font-css.css">
     <link rel="stylesheet" href="/style/main.css">
 
-    <link rel="stylesheet" href="/style/builds/index.build/desktop.css" media="only screen and (min-width: 769px)">
-    <link rel="stylesheet" href="/style/builds/index.build/tablet.css" media="only screen and (min-width: 451px) and (max-width: 768px)">
-    <link rel="stylesheet" href="/style/builds/index.build/mobile.css" media="only screen (max-width: 450px)">
+    <link rel="stylesheet" href="/style/builds/index.build/desktop.css" media="only screen and (min-width: 901px)">
+    <link rel="stylesheet" href="/style/builds/index.build/tablet.css" media="only screen and (min-width: 451px) and (max-width: 900px)">
+    <link rel="stylesheet" href="/style/builds/index.build/mobile.css" media="only screen and (max-width: 450px)">
 </head>
 <body class="body">
-    <header class="header"></header>
+    <header class="header header_theme_classic">
+        <div class="header__container">
+            <div class="header__wrap">
+                <div class="header__top">
+                    <div class="logo-block logo-block_theme_emerald header__logo">
+                        <img class="logo-block__logo" src="/style/style-image/border-logo.png" alt="logo">
+                        <span class="logo-block__brand text">tyrest</span>
+                    </div>
+                    <nav class="navbar navbar_theme_emerald header__navbar">
+                        <a class="anchor btn btn_type_flex navbar__item navbar__item_active">HOME</a>
+                        <a class="anchor btn btn_type_flex navbar__item">ABOUT</a>
+                        <a class="anchor btn btn_type_flex navbar__item">EXPERTISE</a>
+                        <a class="anchor btn btn_type_flex navbar__item">TEAMS</a>
+                        <a class="anchor btn btn_type_flex navbar__item">WORKS</a>
+                        <a class="anchor btn btn_type_flex navbar__item">PEOPLE SAY</a>
+                        <a class="anchor btn btn_type_flex navbar__item">CONTACT</a>
+                    </nav>
+                </div>
+                <div class="slider slider_theme_emerald header__slider">
+                    <div class="slider__content">
+                        <div class="slider__title-container">
+                            <span class="slider__title">Lorem ipsum dolor sit.</span>
+                        </div>
+                        <div class="slider__separator"></div>
+                        <div class="slider__text-container">
+                            <p class="slider__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam atque, blanditiis deleniti doloremque eius enim in molestiae nemo neque nobis, numquam omnis optio pariatur porro repellat sed soluta. Consequatur deleniti dolore officia. Assumenda eius facere in, laborum non quisquam voluptatibus.</p>
+                        </div>
+                        <div class="btn slider__btn-more">
+                            <span class="btn__text btn_type_classic btn_theme_classic-emerald">LEARN MORE</span>
+                        </div>
+                    </div>
+                    <div class="slider__nav">
+                        <div class="slider__nav-item slider__nav-item_active"></div>
+                        <div class="slider__nav-item"></div>
+                        <div class="slider__nav-item"></div>
+                        <div class="slider__nav-item"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
 
     <section class="about"></section>
     <section class="about-video"></section>
